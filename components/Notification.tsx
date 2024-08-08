76f9937fd1386b2da02687b5548cbba38388d2b7
haido86 <thithuhai.do@gmail.com>
2024-08-08 13:41:41 +0300

    Notification ui

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Notification() {
-  return <div>Notification</div>;
+  return (
+    <div className="h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer">
+      Free delivery on orders over $50. Order your food now!
+    </div>
+  );
 }
