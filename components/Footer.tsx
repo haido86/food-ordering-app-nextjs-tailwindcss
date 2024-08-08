ae6060ce1907d074c36dbd1025aff39882017d8d
haido86 <thithuhai.do@gmail.com>
2024-08-08 16:04:16 +0300

    Footer ui

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="uppercase text-red-500 h-12 md:h-24 p-4 lg:px-20 xl:px-40 flex items-center justify-between">
+      <Link href="/" className="font-bold">
+        Massimo
+      </Link>
+      <p>Ⓒ all rights reserved.</p>
+    </div>
+  );
 }
