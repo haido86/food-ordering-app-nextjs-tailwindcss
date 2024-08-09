1a2dd715091d57e4b03e56293b9f1472c8882aea
haido86 <thithuhai.do@gmail.com>
2024-08-09 09:59:19 +0300

    Offer ui

--- components/Offer.tsx
+++ components/Offer.tsx
@@ -1,5 +1,29 @@
+import Image from 'next/image';
 import React from 'react';
 
 export default function Offer() {
-  return <div>Offer</div>;
+  return (
+    <div className="flex flex-col bg-black md:flex-row h-screen">
+      {/* text container */}
+      <div className="text-white text-center flex-1 justify-center items-center flex flex-col p-6 md:p-20 gap-8">
+        <h1 className="font-bold uppercase text-5xl xl:text-6xl 2xl:text-7xl">
+          Delicious Burger & French Fry
+        </h1>
+        <p className="xl:text-xl">
+          Progressively simplify effective e-toilers and process-centric methods
+          of empowerment. Quickly pontificate parallel.
+        </p>
+        <button className="bg-red-500 px-6 py-3 rounded-lg">Order Now</button>
+      </div>
+      {/* image container */}
+      <div className="relative flex-1 w-full">
+        <Image
+          src="/offerProduct.png"
+          alt="pizza"
+          fill
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
 }
